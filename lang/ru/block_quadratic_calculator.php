c47f6a7a698ba24e31e15623f7a0575c6a5e2805
E.V.Komarov <OneDarker@yandex.ru>
2024-04-12 17:44:48 +0700

    Lang ru

--- lang/ru/block_quadratic_calculator.php
+++ lang/ru/block_quadratic_calculator.php
@@ -1,8 +1,9 @@
 <?php
 defined('MOODLE_INTERNAL') || die();
 $string['pluginname'] = 'Калькулятор решения квадратного уравнения';
-$string['quadratic_calculator:addinstance'] = 'Add a new quadratic equation calculator block';
+$string['quadratic_calculator:addinstance'] = 'Добавьте новый блок калькулятора квадратных уравнений';
 $string['coefficienta'] = 'Коэфициент A';
 $string['coefficientb'] = 'Коэфициент B';
 $string['coefficientc'] = 'Коэфициент C';
 $string['calculate'] = 'Рассчитать';
+$string['error_calculate_roots'] = 'У уравнения нет действительных корней';
