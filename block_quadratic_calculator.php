ff94c327da719aea879a86417df8369efe5b743e
E.V.Komarov <OneDarker@yandex.ru>
2024-04-15 22:40:33 +0700

    database + history

--- block_quadratic_calculator.php
+++ block_quadratic_calculator.php
@@ -7,31 +7,40 @@ class block_quadratic_calculator extends block_base {
     }
 
     public function get_content() {
-        global $OUTPUT, $PAGE;
-
-        if ($this->content !== null) {
-            return $this->content;
-        }
-
-        $this->content = new stdClass;
-        $this->content->text = '';
-        $this->content->footer = '';
-
-        // Подключаем JavaScript-модуль для AJAX.
-        $PAGE->requires->js_call_amd(
+      global $OUTPUT, $PAGE;
+  
+      if ($this->content !== null) {
+          return $this->content;
+      }
+  
+      $this->content = new stdClass;
+      $this->content->text = '';
+      $this->content->footer = '';
+  
+      // Подключаем JavaScript-модуль для AJAX.
+      $PAGE->requires->js_call_amd(
           'block_quadratic_calculator/module', 
           'init', 
           array('selector' => '#quadratic-calculator-form')
-        );
-
-        // Подготавливаем данные для шаблона.
-        $data = new stdClass();
-        $data->action_url = new moodle_url('/blocks/quadratic_calculator/calculate.php');
-        $data->sesskey = sesskey();
-
-        // Рендерим форму с использованием шаблона Mustache.
-        $this->content->text = $OUTPUT->render_from_template('block_quadratic_calculator/calculator_form', $data);
-
-        return $this->content;
+      );
+  
+      // Подключаем JavaScript-модуль для модального окна.
+      $PAGE->requires->js_call_amd(
+          'block_quadratic_calculator/modal', 
+          'init'
+      );
+  
+      // Подготавливаем данные для шаблона.
+      $data = new stdClass();
+      $data->action_url = new moodle_url('/blocks/quadratic_calculator/calculate.php');
+      $data->sesskey = sesskey();
+  
+      // Рендерим форму с использованием шаблона Mustache.
+      $this->content->text = $OUTPUT->render_from_template('block_quadratic_calculator/calculator_form', $data);
+  
+      // Добавляем HTML-код кнопки для открытия модального окна.
+      //$this->content->text .= $OUTPUT->render_from_template('block_quadratic_calculator/history', new stdClass());
+  
+      return $this->content;
     }
 }
--- lang/ru/block_quadratic_calculator.php
+++ lang/ru/block_quadratic_calculator.php
@@ -1,8 +1,13 @@
 <?php
 defined('MOODLE_INTERNAL') || die();
 $string['pluginname'] = 'Калькулятор решения квадратного уравнения';
-$string['quadratic_calculator:addinstance'] = 'Add a new quadratic equation calculator block';
+$string['quadratic_calculator:addinstance'] = 'Добавьте новый блок калькулятора квадратных уравнений';
 $string['coefficienta'] = 'Коэфициент A';
 $string['coefficientb'] = 'Коэфициент B';
 $string['coefficientc'] = 'Коэфициент C';
 $string['calculate'] = 'Рассчитать';
+$string['error_calculate_roots'] = 'Предупреждение: у уравнения нет действительных корней';
+$string['block_discription'] = 'Позволяет решать уравнения типа';
+$string['input_title'] = 'Введите число';
+$string['history'] = 'История';
+$string['error_insert_record'] = 'Невозможно вставить запись в базу данных';
